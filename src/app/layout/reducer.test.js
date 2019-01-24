60a2362ba91c942adf2c74fac0cba010eab923bc
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-01-24 21:22:47 -0200

    Add "changeTitle" action reducer test

--- src/app/layout/reducer.test.js
+++ src/app/layout/reducer.test.js
@@ -1,4 +1,5 @@
 import layoutReducer, { layoutInitialState } from "app/layout/reducer";
+import layoutActionsTypes, { changeTitle } from "./actions";
 
 describe("The layout module reducer", () => {
   describe("when an unknown action is provided", () => {
@@ -8,6 +9,48 @@ describe("The layout module reducer", () => {
       const state = layoutInitialState;
       const result = layoutReducer(state, unknownAction);
       expect(result).toBe(state);
+    });
+  });
+
+  describe(`when the "${
+    layoutActionsTypes.CHANGE_TITLE
+  }" action was dispatched`, () => {
+    describe('with "" as title payload', () => {
+      let result;
+
+      beforeEach(() => {
+        const state = layoutInitialState;
+        const action = changeTitle("");
+
+        result = layoutReducer(state, action);
+      });
+
+      it('it reduces to a state with title value = ""', () => {
+        expect(result.title).toBe("");
+      });
+
+      it("it reduces to a different state instance", () => {
+        expect(result).not.toBe(layoutInitialState);
+      });
+    });
+
+    describe('with "Test" as title payload', () => {
+      let result;
+
+      beforeEach(() => {
+        const state = layoutInitialState;
+        const action = changeTitle("Test");
+
+        result = layoutReducer(state, action);
+      });
+
+      it('it reduces to a state with title value = "Test"', () => {
+        expect(result.title).toBe("Test");
+      });
+
+      it("it reduces to a different state instance", () => {
+        expect(result).not.toBe(layoutInitialState);
+      });
     });
   });
 });
