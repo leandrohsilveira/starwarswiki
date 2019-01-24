4e7a5d9b8d8618d05c8044a820ed80f0773a0146
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-01-24 20:23:50 -0200

    Fix reducers refs

--- src/app/layout/reducer.js
+++ src/app/layout/reducer.js
@@ -1,8 +1,6 @@
-import { initialState } from "app/reducers";
-
 export const layoutInitialState = {};
 
-function layoutReducer(state = initialState, { type, ...payload }) {
+function layoutReducer(state = layoutInitialState, { type, ...payload }) {
   switch (type) {
     default:
       return state;
