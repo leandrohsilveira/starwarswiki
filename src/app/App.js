804040dee0b851cdf27bc225a209b7766d69dcdb
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-01-24 20:15:41 -0200

    Use LayoutProvider component in App

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
 import { store } from "app/config";
+import LayoutProvider from "app/layout/providers/LayoutProvider";
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="App">Hello</div>
+        <LayoutProvider>Hello</LayoutProvider>
       </Provider>
     );
   }
