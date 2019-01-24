f7752bcc898c8b5de1a6d13b5e43f9ff55d2b697
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-01-24 21:23:51 -0200

    Add changeTitle action

--- src/app/layout/actions.js
+++ src/app/layout/actions.js
@@ -1,3 +1,10 @@
-const layoutActionsTypes = {};
+const layoutActionsTypes = {
+  CHANGE_TITLE: "[Layout] Change title"
+};
+
+export const changeTitle = title => ({
+  type: layoutActionsTypes.CHANGE_TITLE,
+  title
+});
 
 export default layoutActionsTypes;
