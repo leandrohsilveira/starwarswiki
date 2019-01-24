838e613d1502279e0d072b9c5d377b9ee80df274
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-01-24 21:24:02 -0200

    Reduce changeTitle action

--- src/app/layout/reducer.js
+++ src/app/layout/reducer.js
@@ -1,7 +1,16 @@
-export const layoutInitialState = {};
+import layoutActionsTypes from "./actions";
+
+export const layoutInitialState = {
+  title: ""
+};
 
 function layoutReducer(state = layoutInitialState, { type, ...payload }) {
   switch (type) {
+    case layoutActionsTypes.CHANGE_TITLE:
+      return {
+        ...state,
+        title: payload.title
+      };
     default:
       return state;
   }
