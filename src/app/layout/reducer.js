b665f682210777779392d77273f003c2219031dc
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-01-24 20:05:27 -0200

    Add layout reducer

---
+++ src/app/layout/reducer.js
@@ -0,0 +1,12 @@
+import { initialState } from "app/reducers";
+
+export const layoutInitialState = {};
+
+function layoutReducer(state = initialState, { type, ...payload }) {
+  switch (type) {
+    default:
+      return state;
+  }
+}
+
+export default layoutReducer;
