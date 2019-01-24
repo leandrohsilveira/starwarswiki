1af3bbf69dcf2c1bdad93c0e2522549a237584a1
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-01-24 19:35:50 -0200

    Add redux provider

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import { store } from "app/config";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello
-      </div>
+      <Provider store={store}>
+        <div className="App">Hello</div>
+      </Provider>
     );
   }
 }
