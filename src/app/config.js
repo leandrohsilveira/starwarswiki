e6b23f645956084686ef01999b79660d4c246eda
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-01-24 22:13:21 -0200

    Configure redux devtools

--- src/app/config.js
+++ src/app/config.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { createEpicMiddleware } from "redux-observable";
 
 import reducers, { initialState } from "app/reducers";
@@ -6,10 +6,18 @@ import epics from "app/epics";
 
 const epicMiddleware = createEpicMiddleware();
 
+let composeEnhancers = compose;
+if (
+  process.env.NODE_ENV !== "production" &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+) {
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+}
+
 export const store = createStore(
   reducers,
   initialState,
-  applyMiddleware(epicMiddleware)
+  composeEnhancers(applyMiddleware(epicMiddleware))
 );
 
 epicMiddleware.run(epics);
