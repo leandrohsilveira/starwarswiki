d3bdeb1e54785633a2c66b01e205d3be7942a599
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-01-29 13:41:58 -0200

    Reduce films actions

--- src/app/films/reducer.js
+++ src/app/films/reducer.js
@@ -1,9 +1,16 @@
+import filmsActionsTypes from './actions';
+
 export const filmsInitialState = {
   films: [],
+  loading: false,
 };
 
-function filmsReducer(state = filmsInitialState, { type }) {
+function filmsReducer(state = filmsInitialState, { type, ...payload }) {
   switch (type) {
+    case filmsActionsTypes.LOAD:
+      return { ...state, loading: true };
+    case filmsActionsTypes.LOADED:
+      return { ...state, loading: false, films: payload.films };
     default:
       return state;
   }
