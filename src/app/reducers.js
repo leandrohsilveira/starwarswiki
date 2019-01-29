a02a44a9e38ebb22d7c2a538d1a9bb44639dc785
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-01-29 13:04:30 -0200

    Add films reducer to root reducers

--- src/app/reducers.js
+++ src/app/reducers.js
@@ -1,12 +1,15 @@
 import { combineReducers } from 'redux';
 import layoutReducer, { layoutInitialState } from 'app/layout/reducer';
+import filmsReducer, { filmsInitialState } from './films/reducer';
 
 export const initialState = {
   layout: layoutInitialState,
+  films: filmsInitialState,
 };
 
 const reducers = combineReducers({
   layout: layoutReducer,
+  films: filmsReducer,
 });
 
 export default reducers;
