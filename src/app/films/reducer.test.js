fa1606feeab07b2aeacc7c7cd5891a46cd6f8a39
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-01-29 13:37:18 -0200

    Add loadFilms and filmsLoaded actions tests

--- src/app/films/reducer.test.js
+++ src/app/films/reducer.test.js
@@ -1,4 +1,6 @@
+import layoutActionsTypes from 'app/layout/actions';
 import filmsReducer, { filmsInitialState, filmsSelector } from './reducer';
+import filmsActionsTypes, { loadFilms, filmsLoaded } from './actions';
 
 const featureInitialState = {
   films: filmsInitialState,
@@ -8,10 +10,69 @@ describe('The films module reducer', () => {
   describe('when an unknown action is provided', () => {
     const unknownAction = { type: '=====' };
 
-    it('it reduces to initial state', () => {
+    it('it reduces to same state instance', () => {
       const state = filmsInitialState;
       const result = filmsReducer(state, unknownAction);
       expect(result).toBe(state);
+    });
+  });
+
+  describe(`when a "${layoutActionsTypes.LOAD}" action is provided`, () => {
+    const action = loadFilms();
+
+    describe('and with initial state', () => {
+      it('it reduces to a state with "loading" true', () => {
+        const state = filmsInitialState;
+        const result = filmsReducer(state, action);
+        expect(result.loading).toBeTruthy();
+      });
+    });
+
+    describe('and with a truthy "loading" state', () => {
+      it('it reduces to a state with "loading" true', () => {
+        const state = {
+          ...filmsInitialState,
+          loading: true,
+        };
+        const result = filmsReducer(state, action);
+        expect(result.loading).toBeTruthy();
+      });
+    });
+  });
+
+  describe(`when a "${layoutActionsTypes.LOADED}" action is provided`, () => {
+    const films = [{ name: 'Name' }];
+    const action = filmsLoaded(films);
+
+    describe('and with initial state', () => {
+      it('it reduces to a state with "films" array not empty', () => {
+        const state = filmsInitialState;
+        const result = filmsReducer(state, action);
+        expect(result.films).toEqual(films);
+      });
+
+      it('it reduces to a state with "loading" false', () => {
+        const state = filmsInitialState;
+        const result = filmsReducer(state, action);
+        expect(result.loading).toBeFalsy();
+      });
+    });
+
+    describe('and with a truthy "loading" state', () => {
+      it('it reduces to a state with "films" array not empty', () => {
+        const state = filmsInitialState;
+        const result = filmsReducer(state, action);
+        expect(result.films).toEqual(films);
+      });
+
+      it('it reduces to a state with "loading" true', () => {
+        const state = {
+          ...filmsInitialState,
+          loading: true,
+        };
+        const result = filmsReducer(state, action);
+        expect(result.loading).toBeFalsy();
+      });
     });
   });
 });
