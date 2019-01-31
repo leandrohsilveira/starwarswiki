3ae82ff681054faea6b7b77188148992f39caa35
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-01-31 13:59:56 -0200

    Add root index test

---
+++ src/index.test.js
@@ -0,0 +1,12 @@
+describe('The index file', () => {
+  it('inserts App component into "root" element', (done) => {
+    const div = document.createElement('div');
+    div.id = 'root';
+    document.body.appendChild(div);
+    import('./index')
+      .then(() => {
+        done();
+      })
+      .catch(done.fail);
+  });
+});
