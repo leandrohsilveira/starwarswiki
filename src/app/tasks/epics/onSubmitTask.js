8ef6fa9b26389a593fbb9e299949911aa4d68ea0
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-01-31 13:43:16 -0200

    Add submitTask epic effect

---
+++ src/app/tasks/epics/onSubmitTask.js
@@ -0,0 +1,13 @@
+import { ofType } from 'redux-observable';
+import { filter, map } from 'rxjs/operators';
+import tasksActionsTypes from '../actions';
+
+function onSubmitTask(actions$) {
+  return actions$.pipe(
+    ofType(tasksActionsTypes.SUBMIT),
+    filter(({ task }) => !!task.effect),
+    map(({ task }) => task.effect),
+  );
+}
+
+export default onSubmitTask;
---
+++ src/app/tasks/epics/onSubmitTask.test.js
@@ -0,0 +1,42 @@
+import { BehaviorSubject, combineLatest } from 'rxjs';
+import { take } from 'rxjs/operators';
+import onSubmitTask from './onSubmitTask';
+import tasksActionsTypes, { submitTask } from '../actions';
+import tasksEpics from '.';
+
+describe('onSubmitTask epic', () => {
+  let actions$;
+
+  beforeEach(() => {
+    actions$ = new BehaviorSubject();
+  });
+
+  afterEach(() => {
+    actions$.complete();
+  });
+
+  describe(`when a "${tasksActionsTypes.SUBMIT}" action is provided`, () => {
+    const task = {
+      id: 'fetchUser',
+      name: 'Fetching user data',
+      effect: { type: 'effect action' },
+    };
+    const action = submitTask(task);
+
+    it('it effects to the action of the "effect" prop of the task', (done) => {
+      actions$.next(action);
+      combineLatest(actions$, onSubmitTask(actions$), tasksEpics(actions$))
+        .pipe(take(1))
+        .subscribe(([latestAction, onSubmitTaskEffect, tasksEpicsEffect]) => {
+          try {
+            expect(latestAction.type).toBe(action.type);
+            expect(onSubmitTaskEffect.type).toBe(task.effect.type);
+            expect(tasksEpicsEffect.type).toBe(task.effect.type);
+            done();
+          } catch (e) {
+            done.fail(e);
+          }
+        });
+    });
+  });
+});
