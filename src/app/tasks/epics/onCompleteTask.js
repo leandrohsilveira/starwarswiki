33911d118df0ffed00770d445bed7fac22cec65a
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-02-01 19:51:48 -0200

    Combine "handleClear" and "handleEffect" into "onCompleteTask" epic

--- src/app/tasks/epics/onCompleteTask.js
+++ src/app/tasks/epics/onCompleteTask.js
@@ -1,13 +1,25 @@
-import { ofType } from 'redux-observable';
+import { ofType, combineEpics } from 'redux-observable';
 import { filter, map } from 'rxjs/operators';
-import tasksActionsTypes from '../actions';
+import tasksActionsTypes, { clearTask } from '../actions';
 
-function onCompleteTask(actions$) {
-  return actions$.pipe(
-    ofType(tasksActionsTypes.COMPLETE),
+function onCompleteActions$(actions$) {
+  return actions$.pipe(ofType(tasksActionsTypes.COMPLETE));
+}
+
+function handleEffect(actions$) {
+  return onCompleteActions$(actions$).pipe(
     filter(({ effect }) => !!effect),
     map(({ effect }) => effect),
   );
 }
+
+function handleClear(actions$) {
+  return onCompleteActions$(actions$).pipe(
+    filter(({ successful, contextChanged }) => successful && !contextChanged),
+    map(({ task }) => clearTask(task)),
+  );
+}
+
+const onCompleteTask = combineEpics(handleEffect, handleClear);
 
 export default onCompleteTask;
