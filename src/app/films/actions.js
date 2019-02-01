c04401b12ce9d63c8f7b71aa92d6ff832455300e
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-02-01 12:43:34 -0200

    Add pageable to films actions

--- src/app/films/actions.js
+++ src/app/films/actions.js
@@ -4,13 +4,15 @@ const filmsActionsTypes = {
   FETCH_PAGE: '[Films] Fetch films page',
 };
 
-export const loadFilms = () => ({
+export const loadFilms = pageable => ({
   type: filmsActionsTypes.LOAD,
+  pageable,
 });
 
-export const filmsLoaded = films => ({
+export const filmsLoaded = (films, pageable) => ({
   type: filmsActionsTypes.LOADED,
   films,
+  pageable,
 });
 
 export const fetchFilmsPage = pageable => ({
