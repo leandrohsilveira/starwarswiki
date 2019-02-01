2b5fd0663815906f3e4fb184d505057b423da111
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-02-01 12:45:33 -0200

    Reduce loadFilms action with pageable payload

--- src/app/films/reducer.js
+++ src/app/films/reducer.js
@@ -3,12 +3,17 @@ import filmsActionsTypes from './actions';
 export const filmsInitialState = {
   films: [],
   loading: false,
+  loaded: false,
+  pageable: {
+    page: 0,
+    limit: 10,
+  },
 };
 
 function filmsReducer(state = filmsInitialState, { type, ...payload }) {
   switch (type) {
     case filmsActionsTypes.LOAD:
-      return { ...state, loading: true };
+      return { ...state, loading: true, pageable: payload.pageable };
     case filmsActionsTypes.LOADED:
       return { ...state, loading: false, films: payload.films };
     default:
