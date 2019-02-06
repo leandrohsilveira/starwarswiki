45a99339dd6d37341fd527742de29b802b284b4c
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-02-06 13:02:48 -0200

    Add RXJS test utils

---
+++ src/__test-utils__/rxjs.utils.js
@@ -0,0 +1,15 @@
+import { pipe, of } from 'rxjs';
+import { timeout, catchError } from 'rxjs/operators';
+
+export function notCalledIn(timout) {
+  return pipe(
+    timeout(timout),
+    catchError(() => of('not called')),
+  );
+}
+
+function mockEpic(epic) {
+  return epic.pipe(notCalledIn(300));
+}
+
+export default mockEpic;
