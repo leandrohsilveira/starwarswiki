97490e561eb308016868000d417a66bac905632d
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-02-12 13:30:43 -0200

    Add fetchFilmsPageSuccess action

--- src/app/films/actions.js
+++ src/app/films/actions.js
@@ -2,6 +2,7 @@ const filmsActionsTypes = {
   LOAD: '[Films] Load films',
   LOADED: '[Films] Films loaded',
   FETCH_PAGE: '[Films] Fetch films page',
+  FETCH_SUCCESS: '[Films] Fetch films page SUCCESS',
 };
 
 export const loadFilms = pageable => ({
@@ -17,6 +18,12 @@ export const filmsLoaded = (films, pageable) => ({
 
 export const fetchFilmsPage = pageable => ({
   type: filmsActionsTypes.FETCH_PAGE,
+  pageable,
+});
+
+export const fetchFilmsPageSuccess = (films, pageable) => ({
+  type: filmsActionsTypes.FETCH_SUCCESS,
+  films,
   pageable,
 });
 
