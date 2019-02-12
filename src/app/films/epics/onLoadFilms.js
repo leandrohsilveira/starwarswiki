88ac80f96ca302e9773b3fe61a035993a64a77c5
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-02-12 14:14:56 -0200

    Use "partition" operator to handle onLoadFilms conditionals

--- src/app/films/epics/onLoadFilms.js
+++ src/app/films/epics/onLoadFilms.js
@@ -1,6 +1,6 @@
-import { combineEpics, ofType } from 'redux-observable';
-import { withLatestFrom, map, filter } from 'rxjs/operators';
-import { pipe } from 'rxjs';
+import { ofType } from 'redux-observable';
+import { withLatestFrom, map, partition } from 'rxjs/operators';
+import { pipe, merge } from 'rxjs';
 import { submitTask } from 'app/tasks/actions';
 import { filmsSelector } from '../reducer';
 import filmsActionsTypes, { filmsLoaded, fetchFilmsPage } from '../actions';
@@ -17,46 +17,37 @@ function getFilmsId({ page, limit }) {
   return `films#${page}#${limit}`;
 }
 
-function onLoadFilmsFromLocalStorage(actions$, state$) {
-  return actions$.pipe(
-    ofType(filmsActionsTypes.LOAD),
+function onLoadFilms(actions$, state$) {
+  // filter action type
+  const ofType$ = actions$.pipe(ofType(filmsActionsTypes.LOAD));
+
+  const [memory$, elseLocalStorage$] = ofType$.pipe(
     selectLatestFrom(state$, filmsSelector),
-    filter(([{ pageable }, state]) => !state.films[getFilmsId(pageable)]),
-    map(([{ pageable }]) => [pageable, window.localStorage.getItem(getFilmsId(pageable))]),
-    filter(([, films]) => !!films),
-    map(([pageable, films]) => [pageable, JSON.parse(films)]),
-    map(([pageable, films]) => filmsLoaded(films, pageable)),
+    partition(([{ pageable }, state]) => !!state.films[getFilmsId(pageable)]),
+  );
+
+  const [localStorage$, fetch$] = elseLocalStorage$.pipe(
+    partition(([{ pageable }]) => !!window.localStorage.getItem(getFilmsId(pageable))),
+  );
+
+  return merge(
+    memory$.pipe(
+      map(([{ pageable }, state]) => [pageable, state.films[getFilmsId(pageable)]]),
+      map(([pageable, films]) => filmsLoaded(films, pageable)),
+    ),
+    localStorage$.pipe(
+      map(([{ pageable }]) => [pageable, window.localStorage.getItem(getFilmsId(pageable))]),
+      map(([pageable, films]) => [pageable, JSON.parse(films)]),
+      map(([pageable, films]) => filmsLoaded(films, pageable)),
+    ),
+    fetch$.pipe(
+      map(([{ pageable }]) => submitTask({
+        id: `fetchFilms#${pageable.page}#${pageable.limit}`,
+        name: `Fetching ${pageable.limit} films of page ${pageable.page}`,
+        effect: fetchFilmsPage(pageable),
+      })),
+    ),
   );
 }
-
-function onLoadFilmsFromMap(actions$, state$) {
-  return actions$.pipe(
-    ofType(filmsActionsTypes.LOAD),
-    selectLatestFrom(state$, filmsSelector),
-    map(([{ pageable }, state]) => [pageable, state.films[getFilmsId(pageable)]]),
-    filter(([, films]) => !!films),
-    map(([pageable, films]) => filmsLoaded(films, pageable)),
-  );
-}
-
-function onLoadFilmsFetchNeeded(actions$, state$) {
-  return actions$.pipe(
-    ofType(filmsActionsTypes.LOAD),
-    selectLatestFrom(state$, filmsSelector),
-    filter(([{ pageable }, state]) => !state.films[getFilmsId(pageable)]),
-    filter(([{ pageable }]) => !window.localStorage.getItem(getFilmsId(pageable))),
-    map(([{ pageable }]) => submitTask({
-      id: `fetchFilms#${pageable.page}#${pageable.limit}`,
-      name: `Fetching ${pageable.limit} films of page ${pageable.page}`,
-      effect: fetchFilmsPage(pageable),
-    })),
-  );
-}
-
-const onLoadFilms = combineEpics(
-  onLoadFilmsFromLocalStorage,
-  onLoadFilmsFromMap,
-  onLoadFilmsFetchNeeded,
-);
 
 export default onLoadFilms;
