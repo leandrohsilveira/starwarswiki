934544bd6a064e5d1c4b9e86b5de040194d653cc
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-02-13 09:17:23 -0200

    Add filmsService

---
+++ src/app/films/service.js
@@ -0,0 +1,13 @@
+import { from } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+const filmsService = {
+  fetchPage(pageable = { page: 1, limit: 10 }) {
+    const { page = 1, limit = 10 } = pageable;
+    return from(global.fetch(`/api/films?page=${page}&limit=${limit}`)).pipe(
+      switchMap(response => from(response.json())),
+    );
+  },
+};
+
+export default filmsService;
---
+++ src/app/films/service.test.js
@@ -0,0 +1,156 @@
+import mockFetch from '__test-utils__/fetch.utils';
+import { notCalledIn } from '__test-utils__/rxjs.utils';
+import { take } from 'rxjs/operators';
+import filmsService from './service';
+
+describe('The films service', () => {
+  describe('fetchPage function', () => {
+    const filmsMockP1L10 = [{ name: 'Film 1' }];
+    const filmsMockP2L10 = [{ name: 'Film 11' }];
+    const filmsMockP2L50 = [{ name: 'Film 11' }, { name: 'Film 12' }];
+    beforeAll(() => {
+      jest.spyOn(global, 'fetch').mockImplementation(
+        mockFetch({
+          '/api/films?page=1&limit=10': filmsMockP1L10,
+          '/api/films?page=2&limit=10': filmsMockP2L10,
+          '/api/films?page=2&limit=50': filmsMockP2L50,
+        }),
+      );
+    });
+
+    describe('when called with a pageable with page 1 and limit 10', () => {
+      const pageable = { page: 1, limit: 10 };
+
+      let result$;
+
+      beforeAll(() => {
+        global.fetch.mockClear();
+        result$ = filmsService.fetchPage(pageable);
+      });
+
+      it('it returns an array of films', (done) => {
+        result$
+          .pipe(
+            notCalledIn(100),
+            take(1),
+          )
+          .subscribe((films) => {
+            try {
+              expect(films).toBe(filmsMockP1L10);
+              done();
+            } catch (e) {
+              done.fail(e);
+            }
+          });
+      });
+
+      it('it fetch from "/api/films?page=1&limit=10" URL', (done) => {
+        result$
+          .pipe(
+            notCalledIn(100),
+            take(1),
+          )
+          .subscribe((films) => {
+            try {
+              expect(films).not.toBe('not called');
+              expect(global.fetch).toHaveBeenCalledTimes(1);
+              expect(global.fetch).toHaveBeenCalledWith('/api/films?page=1&limit=10');
+              done();
+            } catch (e) {
+              done.fail(e);
+            }
+          });
+      });
+    });
+
+    describe('when called with a pageable with page 2 and limit 10', () => {
+      const pageable = { page: 2, limit: 10 };
+
+      let result$;
+
+      beforeAll(() => {
+        global.fetch.mockClear();
+        result$ = filmsService.fetchPage(pageable);
+      });
+
+      it('it returns an array of films', (done) => {
+        result$
+          .pipe(
+            notCalledIn(100),
+            take(1),
+          )
+          .subscribe((films) => {
+            try {
+              expect(films).toBe(filmsMockP2L10);
+              done();
+            } catch (e) {
+              done.fail(e);
+            }
+          });
+      });
+
+      it('it fetch from "/api/films?page=2&limit=10" URL', (done) => {
+        result$
+          .pipe(
+            notCalledIn(100),
+            take(1),
+          )
+          .subscribe((films) => {
+            try {
+              expect(films).not.toBe('not called');
+              expect(global.fetch).toHaveBeenCalledTimes(1);
+              expect(global.fetch).toHaveBeenCalledWith('/api/films?page=2&limit=10');
+              done();
+            } catch (e) {
+              done.fail(e);
+            }
+          });
+      });
+    });
+
+    describe('when called with a pageable with page 2 and limit 50', () => {
+      const pageable = { page: 2, limit: 50 };
+
+      let result$;
+
+      beforeAll(() => {
+        global.fetch.mockClear();
+        result$ = filmsService.fetchPage(pageable);
+      });
+
+      it('it returns an array of films', (done) => {
+        result$
+          .pipe(
+            notCalledIn(100),
+            take(1),
+          )
+          .subscribe((films) => {
+            try {
+              expect(films).toBe(filmsMockP2L50);
+              done();
+            } catch (e) {
+              done.fail(e);
+            }
+          });
+      });
+
+      it('it fetch from "/api/films?page=2&limit=50" URL', (done) => {
+        result$
+          .pipe(
+            notCalledIn(100),
+            take(1),
+          )
+          .subscribe((films) => {
+            try {
+              expect(films).not.toBe('not called');
+              expect(global.fetch).toHaveBeenCalledTimes(1);
+              expect(global.fetch).toHaveBeenCalledWith('/api/films?page=2&limit=50');
+              done();
+            } catch (e) {
+              done.fail(e);
+            }
+          });
+      });
+    });
+  });
+});
