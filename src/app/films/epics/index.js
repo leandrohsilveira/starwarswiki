7b8478c2b31e6d8a95c0434da7c4da87f12c7d1d
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-02-13 13:34:07 -0200

    Add onFilmsPageFetched impl

--- src/app/films/epics/index.js
+++ src/app/films/epics/index.js
@@ -1,7 +1,8 @@
 import { combineEpics } from 'redux-observable';
 import onLoadFilms from './onLoadFilms';
 import onFetchFilmsPage from './onFetchFilmsPage';
+import onFilmsPageFetched from './onFilmsPageFetched';
 
-const filmsEpics = combineEpics(onLoadFilms, onFetchFilmsPage);
+const filmsEpics = combineEpics(onLoadFilms, onFetchFilmsPage, onFilmsPageFetched);
 
 export default filmsEpics;
---
+++ src/app/films/epics/onFilmsPageFetched.js
@@ -0,0 +1,29 @@
+import { ofType } from 'redux-observable';
+import { tap, map, partition } from 'rxjs/operators';
+import { merge } from 'rxjs';
+import { completeTask } from 'app/tasks/actions';
+import filmsActionsTypes, { filmsLoaded } from '../actions';
+
+function onFilmsPageFetched(actions$) {
+  const ofType$ = actions$.pipe(
+    ofType(filmsActionsTypes.PAGE_FETCHED),
+    tap(({ films, pageable }) => {
+      const stringify = JSON.stringify(films);
+      const key = `films#${pageable.page}#${pageable.limit}`;
+      window.localStorage.setItem(key, stringify);
+    }),
+    map(({ films, pageable, meta }) => ({
+      effect: filmsLoaded(films, pageable),
+      meta,
+    })),
+  );
+
+  const [taskEffect$, directEffect$] = ofType$.pipe(partition(({ meta }) => meta && meta.task));
+
+  return merge(
+    taskEffect$.pipe(map(({ effect, meta }) => completeTask(meta.task, { effect }))),
+    directEffect$.pipe(map(({ effect }) => effect)),
+  );
+}
+
+export default onFilmsPageFetched;
