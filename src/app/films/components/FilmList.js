64a3a1b3cdd66e3b65c787793d9e20899e2773bf
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-02-14 20:53:25 -0200

    Implement FilmList

--- src/app/films/components/FilmList.js
+++ src/app/films/components/FilmList.js
@@ -1,7 +1,54 @@
 import React from 'react';
+import { arrayOf, shape, number, string } from 'prop-types';
+import { Card, CardBody, CardTitle, CardSubtitle, CardText } from 'reactstrap';
+import { formatDateUTC } from 'app/shared/util/date.util';
 
-function FilmList() {
-  return <div>Not implemented yet</div>;
+function FilmList({ films }) {
+  if (films.length) {
+    return (
+      <>
+        {films.map(
+          ({
+            episode_id: id,
+            title,
+            director,
+            producer,
+            release_date: release
+          }) => (
+            <Card key={id}>
+              <CardBody>
+                <CardTitle>{title}</CardTitle>
+                <CardSubtitle>{formatDateUTC(release)}</CardSubtitle>
+                <CardText>
+                  <strong>Director</strong>: <span>{director}</span>
+                </CardText>
+                <CardText>
+                  <strong>Producer</strong>: <span>{producer}</span>
+                </CardText>
+              </CardBody>
+            </Card>
+          )
+        )}
+      </>
+    );
+  }
+  return <div>No results found</div>;
 }
+
+FilmList.propTypes = {
+  films: arrayOf(
+    shape({
+      episode_id: number,
+      title: string,
+      director: string,
+      producer: string,
+      release_date: string
+    })
+  )
+};
+
+FilmList.defaultProps = {
+  films: []
+};
 
 export default FilmList;
