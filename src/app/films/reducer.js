be61eb2fba1350b34479170ef82359b831e479f6
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-02-13 22:55:24 -0200

    Update filmsReducer impl

--- src/app/films/reducer.js
+++ src/app/films/reducer.js
@@ -2,20 +2,37 @@ import filmsActionsTypes from './actions';
 
 export const filmsInitialState = {
   films: {},
-  loading: false,
-  loaded: false,
   pageable: {
     page: 0,
     limit: 10,
   },
 };
 
+function getPageKey({ page, limit }) {
+  return `${page}#${limit}`;
+}
+
 function filmsReducer(state = filmsInitialState, { type, ...payload }) {
   switch (type) {
     case filmsActionsTypes.LOAD:
-      return { ...state, loading: true, pageable: payload.pageable };
+      return { ...state, pageable: payload.pageable };
+    case filmsActionsTypes.FETCH_PAGE:
+      return {
+        ...state,
+        films: {
+          ...state.films,
+          [getPageKey(payload.pageable)]: false,
+        },
+      };
     case filmsActionsTypes.LOADED:
-      return { ...state, loading: false, films: payload.films };
+      return {
+        ...state,
+        films: {
+          ...state.films,
+          [getPageKey(payload.pageable)]: payload.films,
+        },
+      };
+    case filmsActionsTypes.PAGE_FETCHED:
     default:
       return state;
   }
