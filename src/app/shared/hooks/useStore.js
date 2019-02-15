713ef727a3f875b6620925627524622008e2ccd2
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-02-15 13:49:59 -0200

    Add rxjs variant of useStore hook (useStore$)

--- src/app/shared/hooks/useStore.js
+++ src/app/shared/hooks/useStore.js
@@ -1,9 +1,30 @@
 import { ReactReduxContext } from 'react-redux';
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
+import { Subject } from 'rxjs';
 
 function useStore() {
   const { store, storeState } = useContext(ReactReduxContext);
   return [storeState, store];
+}
+
+export function useStore$(...pipes) {
+  const [storeState, store] = useStore();
+  const [state$] = useState(new Subject(storeState));
+  const [state, setState] = useState(storeState);
+  useEffect(() => {
+    state$.next(storeState);
+  }, [storeState]);
+  useEffect(() => {
+    const subscription = pipes
+      .reduce((observable$, pipe) => observable$.pipe(pipe), state$)
+      .subscribe(nextState => setState(nextState));
+    return () => {
+      subscription.unsubscribe();
+      state$.complete();
+    };
+  }, []);
+
+  return [state, store];
 }
 
 export default useStore;
