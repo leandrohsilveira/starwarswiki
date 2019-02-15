67adcb6c46466feb368fbc96f13cef8a84df03d6
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-02-15 13:49:51 -0200

    Configure browser router

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from 'app/config';
+import { BrowserRouter, Route } from 'react-router-dom';
 import LayoutProvider from 'app/layout/providers/LayoutProvider';
 
 function App() {
   return (
     <Provider store={store}>
-      <LayoutProvider>Hello</LayoutProvider>
+      <BrowserRouter>
+        <Route
+          component={props => <LayoutProvider {...props}>Hello</LayoutProvider>}
+        />
+      </BrowserRouter>
     </Provider>
   );
 }
