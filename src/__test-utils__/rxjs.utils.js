ab403c0099946050bbdbe2ea93714d622b8ab2ed
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-02-15 13:49:32 -0200

    Add combineMockedEpics test util

--- src/__test-utils__/rxjs.utils.js
+++ src/__test-utils__/rxjs.utils.js
@@ -1,5 +1,6 @@
-import { pipe, of } from 'rxjs';
-import { timeout, catchError } from 'rxjs/operators';
+import { pipe, of, BehaviorSubject } from 'rxjs';
+import { timeout, catchError, withLatestFrom, map } from 'rxjs/operators';
+import { combineEpics } from 'redux-observable';
 
 export function notCalledIn(timout) {
   return pipe(
@@ -8,8 +9,31 @@ export function notCalledIn(timout) {
   );
 }
 
+export function mapToStore() {
+  return map(({ store }) => store);
+}
+
 function mockEpic(epic) {
   return epic.pipe(notCalledIn(300));
+}
+
+export function combineMockedEpics(...epics) {
+  const actionSubject = new BehaviorSubject(null);
+
+  function spyEpic(actions$, store$) {
+    return actions$.pipe(
+      withLatestFrom(store$),
+      map(([action, state]) => ({ action, state })),
+      map(store => ({
+        type: '@@epicSpy',
+        store
+      }))
+    );
+  }
+
+  const combinedEpics = mockEpic(combineEpics(spyEpic, ...epics));
+
+  return [combinedEpics, of(actionSubject)];
 }
 
 export default mockEpic;
