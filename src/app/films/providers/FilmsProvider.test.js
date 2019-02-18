e244888afbae58ecc9cecc3c97bc39b90257e4c7
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2019-02-18 20:34:45 -0300

    Improve FilmsProvider component tests

--- src/app/films/providers/FilmsProvider.test.js
+++ src/app/films/providers/FilmsProvider.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { render } from 'react-testing-library';
 import { Provider } from 'react-redux';
-import { combineMockedEpics } from '__test-utils__/rxjs.utils';
+import {
+  combineMockedEpics,
+  NOT_CALLED,
+  spyType
+} from '__test-utils__/rxjs.utils';
 import { createTestStore } from '__test-utils__/redux.utils';
 import tasksEpics from 'app/tasks/epics';
 
@@ -9,6 +13,8 @@ import tasksReducer, { tasksInitialState } from 'app/tasks/reducer';
 import filmsReducer, { filmsInitialState } from '../reducer';
 import filmsEpics from '../epics';
 import FilmsProvider from './FilmsProvider';
+import filmsActionsTypes from '../actions';
+import mockFetch, { mockResult } from '../../../__test-utils__/fetch.utils';
 
 const [epics, spy$] = combineMockedEpics(filmsEpics, tasksEpics);
 
@@ -25,6 +31,20 @@ const store = createTestStore(
 );
 
 describe('FilmsProvider component', () => {
+  beforeAll(() => {
+    mockFetch({
+      '/api/films?page=0&limit=10': mockResult([
+        {
+          episode_id: 1,
+          title: 'Film 1',
+          director: 'Director 1',
+          producer: 'Producer 1',
+          release_date: '2019-01-01'
+        }
+      ])
+    });
+  });
+
   function mount() {
     return render(
       <Provider store={store}>
@@ -32,16 +52,29 @@ describe('FilmsProvider component', () => {
       </Provider>
     );
   }
-
-  it('//TODO:', done => {
-    mount();
-    spy$.subscribe(() => {
+  it('after fetch page action epic, there is a text showing "Loading films..."', done => {
+    const { getByText } = mount();
+    spy$.pipe(spyType(filmsActionsTypes.FETCH_PAGE)).subscribe(effect => {
       try {
-        done.fail('not implemented yet');
+        expect(effect).not.toBe(NOT_CALLED);
+        expect(getByText('Loading films...'));
+        done();
       } catch (e) {
         done.fail(e);
       }
     });
-    store.dispatch({ type: 'Remove this' });
+  });
+
+  it('after page loaded action epic, there is a text showing "Film 1"', done => {
+    const { getByText } = mount();
+    spy$.pipe(spyType(filmsActionsTypes.LOADED)).subscribe(effect => {
+      try {
+        expect(effect).not.toBe(NOT_CALLED);
+        expect(getByText('Film 1'));
+        done();
+      } catch (e) {
+        done.fail(e);
+      }
+    });
   });
 });
